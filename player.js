0acdff40e537f04c9d043cab11a688568d448286
Jason Ma <jason@hicleo.com>
2019-12-19 07:51:32 -0800

    Move player to another file

---
+++ player.js
@@ -0,0 +1,21 @@
+
+function play(world, bird, pipes, fly) {
+  // for (let i = 0; i < pipes.length; i++) {
+  //   let pipe = pipes[i];
+  //   if (pipe.isTop) {
+  //     console.log("top pipe found")
+  //     console.log("x", pipe.x)
+  //     console.log("y", pipe.y)
+  //     console.log("height", pipe.height)
+  //   } else {
+  //     console.log("top pipe found")
+  //     console.log("x", pipe.x)
+  //     console.log("y", pipe.y)
+  //     console.log("height", pipe.height)
+  //   }
+  // }
+
+  // if (world.height - bird.y < 100) {
+  //   fly();
+  // }
+}
